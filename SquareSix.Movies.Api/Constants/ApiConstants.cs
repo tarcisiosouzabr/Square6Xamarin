1c31260d4c9280a487ada1dff16982c7c2958a1a
Tarcisio Souza <tarcisiosouzabr@gmail.com>
2023-06-29 20:39:24 -0300

    http Service

--- SquareSix.Movies.Api/Constants/ApiConstants.cs
+++ SquareSix.Movies.Api/Constants/ApiConstants.cs
@@ -2,6 +2,6 @@
 {
     public const string ApiUrl = "http://api.themoviedb.org/";
     public const string ApiKey = "02827685182e761fd77f3cac07b1bef6";
-    public const string ImageUrl = "https://image.tmdb.org/t/p/w185";
+    public const string ImageUrl = "http://image.tmdb.org/t/p/w185";
     
 }
